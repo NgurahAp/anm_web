7e209ed0a76f2e1c64c499c478cf66fc54f3bbb1
Arya <ngurahpratama2002@gmail.com>
2024-12-16 15:06:28 +0700

    Done our product

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,9 +70,8 @@
             <img src="{{ asset('images/navbar_logo.jpg') }}" alt="Densimeter" class="absolute bottom-12 right-10 w-40">
         </div>
     </section>
-
     {{-- About Us --}}
-    <section class="h-screen flex items-center">
+    <section class="min-h-screen flex items-center">
         <div class="flex gap-x-10 px-24 ">
             <div class="w-1/2">
                 <img class="w-full" src="{{ asset('images/about_us.jpg') }}" alt="Logo">
@@ -166,5 +165,67 @@
             </div>
         </div>
     </section>
+    {{-- Products --}}
+    <section class="min-h-screen my-10 mx-20">
+        <div class="flex flex-col text-center font-urbanist">
+            <p class="text-lg font-extrabold text-green-800 pt-14 pb-5">Produk Unggulan Kami</p>
+            <h1 class="text-5xl font-extrabold text-gray-800 pb-14">Produk Terbaik untuk Kebutuhan <br> Laboratorium
+                dan
+                Kesehatan</h1>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div class="bg-white rounded-lg border hover:border-red-500  overflow-hidden pt-10">
+                <div class="flex justify-center">
+                    <img src="{{ asset('images/goldMeter.jpg') }}" alt="Gold Meter & Precious Metal Tester"
+                        class=" h-56 object-cover">
+
+                </div>
+                <div class="p-6 flex flex-col items-center">
+                    <h1 class="text-2xl text-center font-semibold text-gray-800">Gold Meter & Precious Metal Tester
+                    </h1>
+                    <p class="text-gray-600 text-sm text-center py-5">Terdapat alat Alfa Mirage GKK 3000 dan Gold Meter
+                        GK-300 untuk
+                        akurasi tinggi dalam pengukuran kadar logam mulia.</p>
+                    <button
+                        class="bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded-md mt-4">Pesan
+                        Produk</button>
+                </div>
+            </div>
+            <div class="bg-white rounded-lg border hover:border-red-500  overflow-hidden pt-10">
+                <div class="flex justify-center">
+                    <img src="{{ asset('images/densimeter.jpg') }}" alt="Gold Meter & Precious Metal Tester"
+                        class=" h-56 object-cover">
+
+                </div>
+                <div class="p-6 flex flex-col items-center">
+                    <h1 class="text-2xl text-center font-semibold text-gray-800">Densimeter</h1>
+                    <p class="text-gray-600 text-sm text-center py-5 pb-[5.5rem]">MD-300S untuk pengukuran densitas
+                        yang akurat.
+
+                    </p>
+                    <button
+                        class="bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded-md mt-4">Pesan
+                        Produk</button>
+                </div>
+            </div>
+            <div class="bg-white rounded-lg border hover:border-red-500  overflow-hidden pt-10">
+                <div class="flex justify-center">
+                    <img src="{{ asset('images/densimeter.jpg') }}" alt="Gold Meter & Precious Metal Tester"
+                        class=" h-56 object-cover">
+
+                </div>
+                <div class="p-6 flex flex-col items-center">
+                    <h1 class="text-2xl text-center font-semibold text-gray-800">Mikroskop & Preparat
+                    </h1>
+                    <p class="text-gray-600 text-sm text-center py-5 pb-[4.2rem]">Berbagai jenis mikroskop dan preparat untuk
+                        kebutuhan penelitian.</p>
+                    <button
+                        class="bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded-md mt-4">Pesan
+                        Produk</button>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
+
 </html>
