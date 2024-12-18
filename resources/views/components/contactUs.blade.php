3bbfbeb47f64a6abad3285807f28fb0c3f0ca9f8
Arya <ngurahpratama2002@gmail.com>
2024-12-18 10:56:44 +0700

    Done adding footer icon to sosmed and make it change color when it hovers

--- resources/views/components/contactUs.blade.php
+++ resources/views/components/contactUs.blade.php
@@ -9,14 +9,23 @@
                     berkomitmen memberikan solusi
                     terbaik untuk kebutuhan laboratorium dan kesehatan Anda.</p>
                 <button
-                    class="flex items-center w-80 justify-center border border-red-500 rounded-md py-3 mx-3 my-6 text-red-600 space-x-4">
-                    <img class="h-4 w-4" src="{{ asset('images/call.png') }}" alt="Logo">
-                    <p class=" font-medium">0816-1740-8900</p>
+                    class="flex items-center w-80 justify-center border border-red-500 rounded-md py-3 mx-3 my-6 text-red-600 space-x-4 hover:text-white transition duration-300 hover:bg-red-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                            d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                    </svg>
+                    <span class="font-medium">0816-1740-8900</span>
                 </button>
+
                 <button
-                    class="flex items-center w-80 justify-center border border-red-500 rounded-md py-3 mx-3 my-6 text-red-600 space-x-4">
-                    <img class="h-4 w-4" src="{{ asset('images/mail.png') }}" alt="Logo">
-                    <p class=" font-medium">sales@anm.co.id</p>
+                    class="flex items-center w-80 justify-center border border-red-500 rounded-md py-3 mx-3 my-6 text-red-600 space-x-4 hover:text-white transition duration-300 hover:bg-red-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                    </svg>
+                    <span class="font-medium">sales@anm.co.id</span>
                 </button>
             </div>
             <div class="w-2/3 bg-white rounded-lg p-10">
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="h-[60vh] bg-[#333333] py-16 px-20">
-    <div class="">
+    <div class="flex">
         <div>
             <img src="{{ asset('images/footer_logo.jpg') }}" alt="Gold Meter & Precious Metal Tester" class="w-32">
             <div class="flex py-7">
@@ -7,8 +7,8 @@
                     <img src="{{ asset('images/landing/whatsapp.png') }}" alt="Gold Meter & Precious Metal Tester"
                         class="w-5">
                 </div>
-                <div class="font-urbanist pl-4">
-                    <h1 class="font-bold text-lg text-white">WhatsApp</h1>
+                <div class=" pl-4">
+                    <h1 class="font-urbanist font-bold text-lg text-white">WhatsApp</h1>
                     <p class="text-slate-300 font-medium">0816-1740-8900 / 0813-9838-7525</p>
                 </div>
             </div>
@@ -17,10 +17,59 @@
                     <img src="{{ asset('images/landing/phone.png') }}" alt="Gold Meter & Precious Metal Tester"
                         class="w-5">
                 </div>
-                <div class="font-urbanist pl-4">
-                    <h1 class="font-bold text-lg text-white">Phone</h1>
+                <div class=" pl-4">
+                    <h1 class="font-urbanist font-bold text-lg text-white">Phone</h1>
                     <p class="text-slate-300 font-medium">(021) 8690-6782 / (021) 8690-6783</p>
                 </div>
+            </div>
+        </div>
+        <div class="pl-32">
+            <h1 class="font-urbanist font-medium text-2xl text-white">Alamat Kantor</h1>
+            <p class="text-slate-300 font-light py-4">Jl. Radin Inten II No. 61A Duren Sawit</p>
+            <h1 class="font-urbanist font-medium text-2xl text-white">Email</h1>
+            <p class="text-slate-300 font-light py-4">sales@anm.co.id <br>anugrah.niaga.mandiri@gmail.com</p>
+            <h1 class="font-urbanist font-medium text-2xl text-white">Website</h1>
+            <p class="text-slate-300 font-light pt-4">http://www.anm.co.id</p>
+        </div>
+        <div class="pl-32">
+            <h1 class="font-urbanist font-medium text-2xl text-white">Media Sosial</h1>
+            <div class="flex">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 60" class="w-full">
+                    <g transform="translate(10,10)">
+                        <!-- Facebook Icon -->
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="10" height="10"
+                            class="cursor-pointer stroke-slate-300 hover:stroke-green-700 fill-slate-300 hover:fill-green-700 transition-colors duration-300">
+                            <path
+                                d="M13.396 22v-8.778h2.948l.441-3.406h-3.389V7.598c0-.986.273-1.659 1.676-1.659h1.793V3.005c-.311-.042-1.376-.135-2.617-.135-2.587 0-4.361 1.579-4.361 4.473v2.493H6.346v3.406h2.948V22h4.102z" />
+                        </svg>
+
+                        <!-- Twitter Icon (Lucide) -->
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="10" height="10" x="60"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="cursor-pointer stroke-slate-300 hover:stroke-green-700 fill-slate-300 hover:fill-green-700 transition-colors duration-300">
+                            <path
+                                d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
+                        </svg>
+
+                        <!-- Pinterest Icon -->
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="10" height="10"
+                            x="120"
+                            class="cursor-pointer fill-slate-300 hover:fill-green-700 transition-colors duration-300">
+                            <path
+                                d="M12 0C5.373 0 0 5.373 0 12c0 5.084 3.163 9.426 7.627 11.174-.105-.949-.2-2.405.042-3.441.218-.937 1.407-5.965 1.407-5.965s-.359-.719-.359-1.782c0-1.668.967-2.914 2.171-2.914 1.023 0 1.518.767 1.518 1.69 0 1.029-.655 2.568-.994 3.995-.283 1.194.599 2.169 1.777 2.169 2.133 0 3.772-2.249 3.772-5.495 0-2.873-2.064-4.882-5.012-4.882-3.414 0-5.418 2.561-5.418 5.207 0 1.031.397 2.138.893 2.738a.36.36 0 0 1 .083.343c-.09.375-.293 1.199-.332 1.363-.053.218-.174.265-.402.159-1.499-.698-2.436-2.889-2.436-4.649 0-3.785 2.75-7.262 7.929-7.262 4.163 0 7.398 2.967 7.398 6.931 0 4.136-2.608 7.464-6.227 7.464-1.216 0-2.359-.632-2.75-1.378l-.748 2.853c-.271 1.043-1.002 2.35-1.492 3.146C9.57 23.756 10.748 24 12 24c6.627 0 12-5.373 12-12S18.627 0 12 0z"
+                                transform="scale(0.8)" />
+                        </svg>
+
+                        <!-- Instagram Icon -->
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="10" height="10"
+                            x="180"
+                            class="cursor-pointer fill-slate-300 hover:fill-green-700 transition-colors duration-300">
+                            <path
+                                d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.148 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.148-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.98-6.98.058-1.28.072-1.689.072-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.98-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.406-11.845a1.44 1.44 0 1 0 0 2.881 1.44 1.44 0 0 0 0-2.881z" />
+                        </svg>
+                    </g>
+                </svg>
             </div>
         </div>
     </div>
